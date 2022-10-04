20de508eac6d6915b51b477f221053d0dbf0fcb0
mfernandezfreire <mfernandezfreir.ext@acciona.com>
2022-10-04 10:44:29 +0200

    version 1.0.0-rc lista

--- example/index.tsx
+++ example/index.tsx
@@ -5,7 +5,7 @@ import { ProductCard, ProductImage, ProductTitle, ProductButtons, } from '../.';
 
 const product = {
   id: '1',
-  title: 'Coffe Mug',
+  title: 'Coffe Mug!',
   // img: './coffee-mug.png'
 };
 
